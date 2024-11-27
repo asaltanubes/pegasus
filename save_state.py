68b91aa2886a3a1aeffd71717847e2ffb3711e5a
Raúl Penagos Solórzano <160320297+RaulPenagos@users.noreply.github.com>
2024-11-27 17:01:47 +0100

    Save_state function
    necesita arregalrse que solo guarda la ultima posicion

--- save_state.py
+++ save_state.py
@@ -3,51 +3,37 @@ from astros import AstroList
 # 
 """
 # nombres sol, tierra, luna, ...
-
 sol masa, tierra masa, luna masa, ...
 tiempo, solx, soly, solz, solvx, solf
 """
 
-file = 'positions.output'
-points = AstroList()
-
 
 """
-for i in range(len(points[0].get_astros()))
+for i in range(len(positions[0].get_astros()))
 with open file
 for astrolist
 astro = astrolist.get_astros[i]
 file
 """
 
-def save_points(points: list[AstroList]):
+# a partir de una lista de objetos AstroList
+def save_positions(positions: list[AstroList]):
     """ 
     Saves all data regarding the Astros in a file for every time step
     calculated (an AstroList)
     """
-    for i in range(len(points[0].get_astros())):
-        astrolist = points[0]
-        astro = AstroList.get_astros[i]
-        with open(f"{astrolist()astro.name}.dat",'w') as data_file:
-            data_file.write(astrolist.get_astros()[i].mass + '\n')
-            for astrolist in points:
-                astro = astrolist.get_astros()[i]
-                data_file.write(astro.position +',')
+    # Me gustaria que los datos se guarden en una carpeta
+    for astro in positions[0].get_all_astros(): # Para cada planeta
+        file_name = f"./output_data/{astro.name}.dat"
+        with open(file_name,'w') as data_file:
+            data_file.write(f"{astro.name},  {astro.mass} \n")
+            for astrolist in positions:
+                data_file.write(','.join([str(x) for x in astro.position]))
                 # data_file.write(astro.velocity +',')
                 # data_file.write(astro.potential_energy +',')
                 data_file.write('\n')
+        data_file.close()
 
 
 
-
-
-
-# import numpy as np
-
-# a = np.array[(1,2,3)],
-# with open('ejemplo.txt', 'w') as file:
-#     file.write(a + '\n')
-#     file.write(a + '\n')
-
-# print(file.read())
 
