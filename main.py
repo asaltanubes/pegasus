a964fe53f55f561afacd0285a789452369dffcbe
asaltanubes <56401901+asaltanubes@users.noreply.github.com>
2024-12-06 20:52:28 +0100

    memory update and animation update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from save_state import save_positions
 - update_forces sirve para algo?
 - update_state necesita forces y potential?
 - use velocity read se está teniendo en cuenta?
+- plot sin ejes con fondo negro para el power point
 
 Por alguna razón que escapa completamente a mi entendimiento, Junquera hace esto para la Luna cunado está leyendo el file de los astros:
 
@@ -30,60 +31,75 @@ def main():
 
     verlet = Verlet(params.delta_time)
 
-    final_time = params.delta_time * params.number_steps
-
-    positions = []
-    potential = []
-    kinetic_energy=[]
-    angular_momentum = []
-    com = []
-    times = []
-    astro_positions = [] # Rulo test
+    final_time = params.delta_time * params.number_steps # Rulo test
 
     print(f"Initial conditions \n {astrolist}")
     print()
     print(f"Parameters: \n{params}")
+    print(repr(astrolist.time))
 
-    earth = astrolist.get_astro_by_name("earth")
-    theta0_earth = np.arctan2(earth.position[1], earth.position[0])
+    # earth = astrolist.get_astro_by_name("earth")
+    # theta0_earth = np.arctan2(earth.position[1], earth.position[0])
 
-    animation_states = []
+    astrolist_states = np.array([])
+    step = 0
 
     while astrolist.time < final_time:
-        print(f"{float(astrolist.time/final_time*100)}", end="\r")
+        print(f"Simulation is {float(astrolist.time)/final_time*100:.3f}% complete!", end="\r")
         astrolist = verlet.advance_time(astrolist, params.delta_time+astrolist.time)
-        animation_states.append(astrolist.copy())
-        positions.append([i.position for i in astrolist.get_free_astros()])
-        com.append(astrolist.center_of_mass)
-        kinetic_energy.append(astrolist.kinetic_energy())
-        potential.append(astrolist.potential)
-        astro_positions.append(astrolist.copy())  # Rulo test
+        astrolist_states = np.append(astrolist_states, astrolist.copy())
+        # positions.append([i.position for i in astrolist.get_free_astros()])
+        # com.append(astrolist.center_of_mass)
+        # kinetic_energy.append(astrolist.kinetic_energy())
+        # potential.append(astrolist.potential)
+        # astro_positions.append(astrolist.copy())  # Rulo test
 
-        angular_momentum.append(astrolist.angular_momentum())
+        # angular_momentum.append(astrolist.angular_momentum())
 
-        times.append(astrolist.time)
-        theta_earth = np.arctan2(earth.position[1], earth.position[0])
+        # times.append(astrolist.time)
+        # theta_earth = np.arctan2(earth.position[1], earth.position[0])
         # if np.abs(theta0_earth-theta_earth) < np.pi/(360*12):
         #     print(f"New year!: {astrolist.time/(3600*24)} day")
-    print()
+        step += 1
+    print("                                           ", end = "\r")
+    print("Simulation complete!")
 
-    save_positions(astro_positions) # Saves all positions in different files for each astro
-
-    potential = np.array(potential)
-    kinetic_energy = np.array(kinetic_energy)
-    total_energy = potential+kinetic_energy
-    x_angular_momentum = [a[0] for a in angular_momentum]
-    y_angular_momentum = [a[1] for a in angular_momentum]
-    z_angular_momentum = [a[2] for a in angular_momentum]
-
-    print(f"Variation of the energy: {(np.max(total_energy)-np.min(total_energy))/(np.mean(total_energy))}")
+    # save_positions([[i.position for i in astrolist.get_all_astros()] for astrolist in astrolist_states]) # Saves all positions in different files for each astro
+    times = np.array([astrolist.time for astrolist in astrolist_states])
+    
+    angular_momentum = np.array([astrolist.angular_momentum() for astrolist in astrolist_states])
+    x_angular_momentum = np.array([a[0] for a in angular_momentum])
+    y_angular_momentum = np.array([a[1] for a in angular_momentum])
+    z_angular_momentum = np.array([a[2] for a in angular_momentum])
+    
     print(f"Max of the angular momentum x component: {np.max(x_angular_momentum)}, minumum of the angular momentum x component: {np.min(x_angular_momentum)}")
     print(f"Max of the angular momentum y component: {np.max(y_angular_momentum)}, minumum of the angular momentum y component: {np.min(y_angular_momentum)}")
     print(f"Variation of the angular momentum z component: {(np.max(z_angular_momentum)-np.min(z_angular_momentum))/(np.mean(z_angular_momentum))}")
+    
+    plt.plot(times, (z_angular_momentum-np.mean(z_angular_momentum))/np.mean(z_angular_momentum))
+    plt.title('Relative deviation of\n the z component of total Angular momentum')
+    plt.show()
+    
+    angular_momentum = None
+    x_angular_momentum = None
+    y_angular_momentum = None
+    z_angular_momentum = None
 
-    with open("positions.output", "wt") as file:
-        file.write("\n".join([", ".join([str(j) for j in i]) for i in positions]))
-    for i in range(len(astrolist.get_free_astros())):
+
+    total_energy = np.array([astrolist.potential for astrolist in astrolist_states]) + np.array([astrolist.kinetic_energy() for astrolist in astrolist_states])
+    
+    print(f"Variation of the energy: {(np.max(total_energy)-np.min(total_energy))/(np.mean(total_energy))}")
+    plt.plot(times, (total_energy-np.mean(total_energy))/np.mean(total_energy))
+    plt.title('Relative deviation of Energy')
+    plt.show()
+    
+    total_energy = None
+
+    positions = [[i.position for i in astrolist.get_all_astros()] for astrolist in astrolist_states]
+    com = [astrolist.center_of_mass for astrolist in astrolist_states]
+    # with open("positions.output", "wt") as file:
+    #     file.write("\n".join([", ".join([str(j) for j in i]) for i in positions]))
+    for i in range(len(astrolist_states[0].get_free_astros())):
         plt.plot([j[i][0] for j in positions], [j[i][1] for j in positions])
     plt.plot([i[0] for i in com], [i[1] for i in com], marker="o")
     plt.gca().set_aspect('equal')
@@ -92,14 +108,11 @@ def main():
     plt.plot([i[0] for i in com], [i[1] for i in com])
     plt.title("COM")
     plt.show()
-
-    plt.plot(times, (total_energy-np.mean(total_energy))/np.mean(total_energy))
-    plt.title('Relative deviation of Energy')
-    plt.show()
-    plt.plot(times, (z_angular_momentum-np.mean(z_angular_momentum))/np.mean(z_angular_momentum))
-    plt.title('Relative deviation of\n the z component of total Angular momentum')
-    plt.show()
-    create_animation(animation_states[::params.animation_step], "animation")
+    positions = None
+    com = None
+    
+    
+    create_animation(astrolist_states[::params.animation_step], "animation")
 
 if __name__ == "__main__":
     main()
